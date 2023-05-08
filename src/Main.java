552b7a0d5ea3633ca8cab70fb229b754755e4f76
kkocaturk <kocaturk.kerim@gmail.com>
2023-05-08 09:52:19 +0300

    git ekleme

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java Candır!");
+        String       ogrenciIsmi            =                "Matrix     ."   ;
+        //DataType   variableName    assigmentOperator          value
+        System.out.println(ogrenciIsmi);
+        ogrenciIsmi="kerim";
+        ogrenciIsmi="ahmet";
+        System.out.println(ogrenciIsmi);
+    }
+}
