06f50980d4e3e170e4ecfc7a25c6a8fed666ae68
kkocaturk <kocaturk.kerim@gmail.com>
2023-05-09 13:51:55 +0300

    2nd day

--- src/day2/C10_Scanner.java
+++ src/day2/C10_Scanner.java
@@ -2,38 +2,23 @@ package day2;
 
 import java.util.Scanner;
 
-public class C09_Scanner {
+public class C10_Scanner {
     public static void main(String[] args) {
         /*
-        Kullanıcıdan iki sayı alıp ikisinin değerlerini değiştirin
-        girilen s1=10 s2=20 ise sonuçta s1=20 s2=10 yapın
+      kullanıcıdan 1 double 1 int alıp toplamını ve çarpımını yazdırın
 
          */
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("sayı1 girin: ");
-        int sayi1 = scanner.nextInt();
-        System.out.println("sayı2 girin: ");
-        int sayi2 = scanner.nextInt();
+        System.out.println("double gir");
+        double girilenDouble = scanner.nextDouble();
 
-        // su kum boş kova
+        System.out.println("int gir");
+        int girilenInt = scanner.nextInt();
 
-        int temp;
+        System.out.println("sayıların toplamı: " + (girilenInt + girilenDouble)  //paranteze almazsak String oalrak düşünüyor.
+                          +  "\nsayıların çarpımı: " + (girilenInt * girilenDouble));
 
-        //1.adım sayi2 temp'e ata
-
-        temp=sayi2;
-
-        //sayi1 'i sayi2'ye atayalım
-
-        sayi2=sayi1;
-
-        //temp değeri sayi1'e atayalım
-
-        sayi1=temp;
-        System.out.println("sayıların değerlerini yerdeğştirdim.");
-        System.out.println("sayi1: " + sayi1);
-        System.out.println("sayi2: " + sayi2);
 
     }
 }
