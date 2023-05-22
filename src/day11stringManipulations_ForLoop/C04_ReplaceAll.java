dfb3c9bade4de26410f8170f7cd8e6d83bc50ff0
kkocaturk <kocaturk.kerim@gmail.com>
2023-05-22 15:54:33 +0300

    12

--- src/day11stringManipulations_ForLoop/C04_ReplaceAll.java
+++ src/day11stringManipulations_ForLoop/C04_ReplaceAll.java
@@ -19,7 +19,9 @@ public class C04_ReplaceAll {
             \\s:space                   \\S:space olmayan herşey
             \\s+ yanyana birden fazla space
             \\d digits                  \\D digit olmayan herşey
-            \\w harf veya rakam         \\W harf veya rakam olmayan herşey
+            \\w harfler, sayılar ve _         \\W harf veya rakam olmayan herşey
+
+            - replaceAll'da regex kullanılır ama char kullanılmaz.
 
          */
         System.out.println("str = " + str);
@@ -32,23 +34,24 @@ public class C04_ReplaceAll {
 
         str = "J1a23va34 5C54and65ir87";
         str = str.replaceAll("\\s","");
-        System.out.println("str'in yeni hali -space : \"\\\\s\" " + str); //123345546587
+        System.out.println("str'in yeni hali -space : \"\\\\s\" " + str); //J1a23va345C54and65ir87
 
         str = "J1a23va34 5C54and65ir87";
         str = str.replaceAll("\\s+","");
-        System.out.println("str'in yeni hali -yanyana birdenfazlaSpace: \"\\\\s+\" " + str); //123345546587
+        System.out.println("str'in yeni hali -yanyana birdenfazlaSpace: \"\\\\s+\" " + str); //J1a23va345C54and65ir87
 
         str = "J1a23va34 5C54and65ir87";
         str = str.replaceAll("\\S","");
-        System.out.println("str'in yeni hali : -spaceOlmayanHerşey \"\\\\s\":  " + str); //123345546587
+        System.out.println("str'in yeni hali : -spaceOlmayanHerşey \"\\\\S\":  " + str); //
 
-        str = "J1a23va34 5C54and65ir87";
+        str = "J1a23va34 5C54and65ir87++-=)(" ;
+        System.out.println(str);
         str = str.replaceAll("\\w","");
-        System.out.println("str'in yeni hali : -harfVeyaRakam \"\\\\w\": " + str); //123345546587
+        System.out.println("str'in yeni hali : -harfVeyaRakam \"\\\\w\": " + str); //
 
         str = "J1a23va34 5C54and65ir87";
         str = str.replaceAll("\\W","");
-        System.out.println("str'in yeni hali :  -harfVeyaRakamOlmayanHerşey \"\\\\W\": " + str); //123345546587
+        System.out.println("str'in yeni hali :  -harfVeyaRakamOlmayanHerşey \"\\\\W\": " + str); // J1a23va345C54and65ir87
 
     }
 }
---
+++ src/day11stringManipulations_ForLoop/C08_forloop.java
@@ -0,0 +1,28 @@
+package day11stringManipulations_ForLoop;
+
+public class C08_forloop {
+    public static void main(String[] args) {
+        // 10 kere "Java candir" yazdirin
+
+        /*
+        System.out.println("Java Candir");
+        System.out.println("Java Candir");
+        System.out.println("Java Candir");
+        System.out.println("Java Candir");
+        System.out.println("Java Candir");
+        System.out.println("Java Candir");
+        System.out.println("Java Candir");
+        System.out.println("Java Candir");
+        System.out.println("Java Candir");
+        System.out.println("Java Candir");
+
+         */
+
+        for (int i = 1; i <=10 ; i++) {
+
+            System.out.println(i +"- Java Candir");
+        }
+
+
+    }
+}
