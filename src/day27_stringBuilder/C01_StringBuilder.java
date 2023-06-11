f8e59a38530e1e0889ecb2410d8056e8bb698586
kkocaturk <kocaturk.kerim@gmail.com>
2023-06-11 22:06:31 +0300

    tarih çalışması

--- src/day27_stringBuilder/C01_StringBuilder.java
+++ src/day27_stringBuilder/C01_StringBuilder.java
@@ -58,3 +58,39 @@ public class C01_StringBuilder {
 
     }
 }
+/*
+Bu program, StringBuilder sınıfının bazı özelliklerini ve işlevlerini göstermektedir.
+İşlevleri açıkladığımızda:
+
+sb1 adında bir StringBuilder nesnesi oluşturulur.
+
+sb1.capacity() metodu 16 döndürür çünkü varsayılan başlangıç kapasitesi 16'dır.
+sb1.length() metodu 0 döndürür çünkü başlangıçta hiç karakter eklenmemiştir.
+sb1'e "Mustafa" eklenir.
+
+sb1.capacity() metodu yine 16 döndürür çünkü kapasite otomatik olarak genişletilmez.
+sb1.length() metodu 7 döndürür çünkü "Mustafa" 7 karakter içerir.
+sb1'e " Yilmazturk" eklenir.
+
+sb1.length() metodu 18 döndürür çünkü "Mustafa Yilmazturk" 18 karakter içerir.
+sb1.capacity() metodu 34 döndürür çünkü yeni kapasite eski kapasitenin iki katı artı iki olur.
+sb1'e " javayi cok sever, sabah aksam Java calisir." eklenir.
+
+sb1.capacity() metodu 70 döndürür çünkü yeni kapasite eski kapasitenin iki katı artı iki olur.
+sb1.length() metodu 62 döndürür çünkü tüm eklenen metin toplamda 62 karakter içerir.
+sb2 adında başlangıç değeri "Java Candir" olan bir StringBuilder nesnesi oluşturulur.
+
+sb2.capacity() metodu 27 döndürür çünkü başlangıç metni 11 karakter içerir ve başlangıç kapasitesi 27'dir.
+sb3 adında başlangıç kapasitesi 11 olan bir StringBuilder nesnesi oluşturulur.
+
+sb3.capacity() metodu 11 döndürür çünkü başlangıç kapasitesi belirtilmiştir.
+sb3.length() metodu 11 döndürür çünkü "54464473321" 11 karakter içerir.
+sb3'e " : Tc No" eklenir.
+
+sb3.capacity() metodu 24 döndürür çünkü yeni kapasite, eski kapasitenin iki katı artı iki olur.
+sb3.length() metodu 19 döndürür çünkü metin toplamda 19 karakter içerir.
+str değişkeni "Java Candir" olarak tanımlanır.
+
+sb3.append(str, 4, str.length()) ifadesi ile str'nin 4. karakterinden sonuna kadar olan bölümü sb3'e eklenir.
+sb3'ün son hali "54464473321
+ */
--- src/day27_stringBuilder/C04_StringBuilderdeStringMethodlari.java
+++ src/day27_stringBuilder/C04_StringBuilderdeStringMethodlari.java
@@ -37,3 +37,33 @@ public class C04_StringBuilderdeStringMethodlari {
 
     }
 }
+/*
+StringBuilder nesnesi üzerinde String metotlaryla işlem yapılmaz.
+Kod olarak yazılırsa ignore eder.
+StringBuilder önce String'e çevirmeli işlem yapılmalıdır.
+Ancak unutulmamalıdır ki String üzerinde yapılan değişiklikler atanmadan string değer üzerine güncelleme yapmaz.
+
+1 StringBuilder nesneleri sb1 ve sb2 oluşturulur ve başlangıç değerleri atanır.
+
+2 sb1.substring(5) ifadesi, sb1'in 5. indexten sonraki alt dizisini elde eder.
+ Ancak bu alt dizeyi yakalamadan doğrudan sb1'in değeri ekrana yazdırılır.
+ Bu durumda, substring() metodu alt dizeyi döndürse de sb1'in değeri değişmeden "Java Candir" olarak görüntülenir.
+
+3 System.out.println(sb1) ifadesiyle sb1'in değeri ekrana yazdırılır.
+ Sonuç olarak, sb1'in değeri değişmeden "Java Candir" olarak görüntülenir.
+
+4 sb1.compareTo(sb2) ifadesi, sb1 ve sb2'yi karşılaştırır ve sonuç olarak -32 döner.
+ Bu, Unicode değerlerine dayalı olarak farklılık gösterir.
+
+5 System.out.println(sb1.toString().equalsIgnoreCase(sb2.toString())) ifadesi, sb1 ve sb2'yi String'e dönüştürdükten sonra karşılaştırır ve sonuç olarak true döner.
+ equalsIgnoreCase() metodu, büyük-küçük harf duyarsız karşılaştırma yapar.
+
+6 sb1.subSequence(3, 6) ifadesiyle sb1 üzerinde 3. indexten başlayarak 6. indexe kadar olan karakterlerin alt dizisi elde edilir.
+ Ancak bu alt dizeyi yakalamadan doğrudan sb1'in değeri ekrana yazdırılır.
+ Bu durumda da subSequence() metodu alt dizeyi döndürse de sb1'in değeri değişmeden "Java Candir" olarak görüntülenir.
+
+7 System.out.println(sb1) ifadesiyle sb1'in değeri ekrana yazdırılır.
+ Sonuç olarak, sb1'in değeri değişmeden "Java Candir" olarak görüntülenir.
+
+Bu örnekleme, StringBuilder nesnesi üzerinde String metotlarının doğrudan kullanılamayacağını ve önce StringBuilder'ı String'e dönüştürmemiz gerektiğini göstermektedir. Ancak, unutulmaması gereken önemli bir nokta, String üzerinde yapılan değişikliklerin doğrudan StringBuilder nesnesine atanmadan orijinal StringBuilder nesnesini değiştirmeyeceğidir
+ */
