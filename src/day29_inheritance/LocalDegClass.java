0317b37d9795774b183ce85ff20df4cfb9e27f76
kkocaturk <kocaturk.kerim@gmail.com>
2023-06-13 12:39:55 +0300

    local2

--- src/day29_inheritance/LocalDegClass.java
+++ src/day29_inheritance/LocalDegClass.java
@@ -2,6 +2,8 @@ package day29_inheritance;
 
 public class LocalDegClass {
     public static void main(String[] args) {
+        System.out.println("2.Lokal değişiklik merge etmeden");
         System.out.println("localde değişilikler yaptım.");
+
     }
 }
